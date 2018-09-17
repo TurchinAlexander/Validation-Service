063df9a07751bb0cac031e61a52414d2d6a1f221
Turchin Alexander <alexanderturchin1@gmail.com>
2018-09-17 23:25:08 +0300

    Added logger to my project.

---
+++ Validation_Service/Logger/ILogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation_Service.Logger
+{
+    /// <summary>
+    /// Interface for interaction with Logger.
+    /// </summary>
+    interface ILogger
+    {
+        /// <summary>
+        /// Logging information.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        void Log(string message);
+    }
+}
---
+++ Validation_Service/Logger/ValidationLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Validation_Service.Logger
+{
+    /// <summary>
+    /// Class to write validation report to file.
+    /// </summary>
+    public class ValidationLogger : ILogger
+    {
+        private const string _extension = "txt";
+
+        /// <summary>
+        /// Name of log file.
+        /// </summary>
+        public string LogName { get; set; } = "log";
+
+        public ValidationLogger(string logName = null)
+        {
+            if (logName != null)
+            {
+                this.LogName = logName;
+            }
+        }
+
+        /// <summary>
+        /// Write message to the file.
+        /// </summary>
+        /// <param name="message">The message</param>
+        public void Log(string message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter($"{LogName}.{_extension}", append: true))
+            {
+                streamWriter.WriteLine($"{DateTime.Now.ToString("G")} : {message}");
+                streamWriter.Close();
+            }
+        }
+
+    }
+}
